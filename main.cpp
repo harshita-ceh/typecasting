b50f417337bfe73acac72e07bfb4ee925b636367
harshita <71144962+harshita-ceh@users.noreply.github.com>
2022-02-28 13:18:55 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int x=10;
+ short y=5;
+ float z;
+ y=x;
+ z = (float)x/2;
+ cout<<"value of x"<<x<<endl;
+ cout<<"value of y"<<y<<endl;
+ cout<<"value of z "<<z<<endl;
+ }
